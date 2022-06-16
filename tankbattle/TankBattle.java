11dd6972af9d6ef3c2fa0f0dd7570f9dbe968935
Max Zhang <55909399+maxzhang725@users.noreply.github.com>
2022-06-15 19:24:00 -0700

    Update TankBattle.java

--- tankbattle/TankBattle.java
+++ tankbattle/TankBattle.java
@@ -19,11 +19,11 @@ public class TankBattle {
         // TODO code application logic here
         JFrame screen = new JFrame();
         Game game = new Game();
-        
+      
         //sets up the ui for game
         screen.setBounds(10, 10, 800, 630);
         screen.setTitle("Tank Battle");
-        screen.setBackground(Color.GREEN);
+        screen.setBackground(Color.WHITE);
         screen.setResizable(false); //maybe dont need
         screen.add(game);
         screen.setVisible(true);
